aee5018865b7257d8bdf38efdf16a2b0dd576f11
Krysson <mgulla@krysson.com>
2024-09-05 10:00:24 -0400

    feat: added done feature -> now have CRUD in Cline

--- todo_app/todolist.py
+++ todo_app/todolist.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-  user_action = input("Type 'add' to add a todo, 'view' to view your todos, 'edit' to edit a todo or 'quit' to quit: ")
+  user_action = input("Enter 'add', 'view', 'edit', 'done' to complete a task or 'quit' to quit: ")
   user_action = user_action.strip()
 
   match  user_action:
@@ -9,13 +9,16 @@ while True:
         todo = input("Enter a todo: ")
         todos.append(todo)
       case 'view' | 'show':
-        for todo in todos:
-          print(todo.capitalize())
+        for index, todo in enumerate(todos):
+            print(f"{index + 1}. {todo.capitalize()}")
       case 'edit' | 'e':
         number = int(input("Number of the todo to edit: "))
         number = number - 1
         new_todo = input("Enter the new todo: ")
-        todos[number] = new_todo        
+        todos[number] = new_todo
+      case 'complete' | 'done':
+        number = int(input("Number of the list to complete?: "))
+        todos.pop(number - 1)
       case 'quit' | 'q':
         break
 
